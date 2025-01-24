cef9494a7ab01f878d03833c90e18b08d2335e4a
Andre Luis Correa <andreluiscorrea2003@gmail.com>
2025-01-24 11:43:41 -0300

    fiz: fix merge conflicts

--- src/main/java/com/project/esii/project_esii/ProjectEsiiApplication.java
+++ src/main/java/com/project/esii/project_esii/ProjectEsiiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventsApplication {
+public class ProjectEsiiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EventsApplication.class, args);
+		SpringApplication.run(ProjectEsiiApplication.class, args);
 	}
 
 }
